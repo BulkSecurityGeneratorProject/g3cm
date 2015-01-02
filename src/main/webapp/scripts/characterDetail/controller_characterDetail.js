92dd9444b6b8dc5f7423e4f8ead93dfea0a770fa
Kyle <kazrap@gmail.com>
2015-01-02 16:13:15 -0600

    begining

---
+++ src/main/webapp/scripts/characterDetail/controller_characterDetail.js
@@ -0,0 +1,35 @@
+'use strict';
+
+g3charactermanagerApp.controller('CharacterController', function ($scope, resolvedCharacter, Character, resolvedCampaign, resolvedCharacterAdvantage, resolvedCharacterSkill, resolvedForm) {
+
+        $scope.characters = resolvedCharacter;
+        $scope.campaigns = resolvedCampaign;
+        $scope.characterAdvantages = resolvedCharacterAdvantage;
+        $scope.characterSkills = resolvedCharacterSkill;
+        $scope.forms = resolvedForm;
+
+        $scope.create = function () {
+            Character.save($scope.character,
+                function () {
+                    $scope.characters = Character.query();
+                    $('#saveCharacterModal').modal('hide');
+                    $scope.clear();
+                });
+        };
+
+        $scope.update = function (id) {
+            $scope.character = Character.get({id: id});
+            $('#saveCharacterModal').modal('show');
+        };
+
+        $scope.deleteCharacter = function (id) {
+            Character.delete({id: id},
+                function () {
+                    $scope.characters = Character.query();
+                });
+        };
+
+        $scope.clear = function () {
+            $scope.character = {name: null, age: null, description: null, bio: null, id: null};
+        };
+    });
